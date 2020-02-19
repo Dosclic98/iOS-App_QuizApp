657eadccb3b6f8c3b03ed478cac32abcb50b9819
Davide Savarro <dosclic98@Davides-iMac.local>
2020-02-19 16:04:12 +0100

    Inizio esercizio

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Davide Savarro on 19/02/2020.
+//  Copyright © 2020 Davide Savarro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
